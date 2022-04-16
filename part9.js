0a4dc5cffb093ae4c733db53e57d34fc24688902
SR00000 <40726606+SR00000@users.noreply.github.com>
2022-04-15 22:14:31 -0400

    First commit

---
+++ part9.js
@@ -0,0 +1,19 @@
+var fname1 = "Jose";
+var lname1 = "Johnson";
+var age1 = "26";
+var height1 = "175";
+var petname1 = "Sammy";
+
+var fname2 = prompt("Hello and Welcome. Please enter your first name:");
+var lname2 = prompt("Please enter your Last Name:");
+var age2 = prompt("How old are you?");
+var height2 = prompt("How tall are you in centimeters?");
+var petname2 = prompt("What is the name of your pet?");
+
+
+alert("Thank you so much for the information")
+ if (fname1 === fname2,lname1 === lname2,age1 === age2,height1 === height2,petname1 === petname2) {
+   console.log("Congrats Comrade! You have passed the spy test!");
+ }else {
+   console.log("Sorry nothing to see here.");
+ }
