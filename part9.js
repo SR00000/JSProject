fd487d15cc7aff04abd1573558faa3562f7e70cd
SR00000 <40726606+SR00000@users.noreply.github.com>
2022-04-16 21:15:32 -0400

    update

--- part9.js
+++ part9.js
@@ -1,18 +1,49 @@
-var fname1 = "Jose";
-var lname1 = "Johnson";
-var age1 = "26";
-var height1 = "175";
-var petname1 = "Sammy";
 
-var fname2 = prompt("Hello and Welcome. Please enter your first name:");
-var lname2 = prompt("Please enter your Last Name:");
-var age2 = prompt("How old are you?");
-var height2 = prompt("How tall are you in centimeters?");
-var petname2 = prompt("What is the name of your pet?");
-
-
+var firstName = prompt("Hello and Welcome. Please enter your first name:");
+var lastName = prompt("Please enter your Last Name:");
+var age = prompt("How old are you?");
+var height = prompt("How tall are you in centimeters?");
+var petName = prompt("What is the name of your pet?");
 alert("Thank you so much for the information")
- if (fname1 === fname2,lname1 === lname2,age1 === age2,height1 === height2,petname1 === petname2) {
+
+
+// Four conditions
+var nameCond = null;
+var ageCond = null;
+var heightCond = null;
+var petCond = null;
+
+// name condition
+if (firstName[0] === lastName[0]) {
+  nameCond = true;
+}else {
+  nameCond = false;
+}
+
+// age condition
+if (age > 20 && age < 30){
+  ageCond = true;
+}else {
+  ageCond = false;
+}
+
+// height condition
+if (height >= 170){
+  heightCond = true;
+}else {
+  heightCond = false;
+}
+
+// pet condition
+if (petName[petName.length-1] === "y") {
+  petCond = true;
+}else {
+  petCond = false;
+}
+
+// check all conditions
+
+ if (nameCond && ageCond && heightCond && petCond ) {
    console.log("Congrats Comrade! You have passed the spy test!");
  }else {
    console.log("Sorry nothing to see here.");
